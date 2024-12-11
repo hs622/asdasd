8c8e95e5c6fceefc86470b97bc61b976b337d9ae
Hussain Ali <50552217+hs622@users.noreply.github.com>
2024-12-11 14:09:35 +0500

    auth cards

---
+++ src/components/cards/auth.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import React from "react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "../ui/card";
+import { Button } from "../ui/button";
+import LoginForm from "../forms/login";
+import { useSearchParams } from "next/navigation";
+import RegisterForm from "../forms/register";
+
+const Auth = () => {
+  const params = useSearchParams();
+
+  return (
+    <div>
+      { params.has("form") && params.get("form") == "signup" ? (
+        <Card className="mx-auto max-w-sm">
+        <CardHeader>
+          <CardTitle className="text-2xl">Register</CardTitle>
+          <CardDescription>
+          Enter your email below to login to your account
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <RegisterForm />
+        </CardContent>
+      </Card>
+      ) : (
+        <Card className="mx-auto max-w-sm">
+          <CardHeader>
+            <CardTitle className="text-2xl">Login</CardTitle>
+            <CardDescription>
+              Enter your email below to login to your account
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <LoginForm />
+          </CardContent>
+        </Card>
+      )}
+    </div>
+  );
+};
+
+export default Auth;
