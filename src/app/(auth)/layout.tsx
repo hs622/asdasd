eb36333909fb58ec34d27f99739f0f4ab5be344b
Hussain Ali <50552217+hs622@users.noreply.github.com>
2025-01-12 22:56:13 +0500

    auth check for guest user.

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,15 +1,22 @@
 import { Metadata } from "next";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../api/auth/[...nextauth]/options";
+import { permanentRedirect } from "next/navigation";
 
 export const metadata: Metadata = {
   title: "Authorization",
   description: "Healthdesk - Authorization",
 };
 
-export default function AuthLayout({
+export default async function AuthLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const response = await getServerSession(authOptions);
+  if(response != null)
+    permanentRedirect('/healthboard');
+
   return (
     <div className="flex justify-center items-center min-h-screen">
       {children}
