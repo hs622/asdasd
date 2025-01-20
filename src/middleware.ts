34fe3d89b78590477e9ebdb56d9035fccf4f6ca6
Hussain Ali <50552217+hs622@users.noreply.github.com>
2025-01-20 08:14:16 +0500

    middleware enabled.

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,9 @@
-// import { NextRequest, NextResponse } from "next/server";
-
-// export function middleware(request: NextRequest) {
-//   return NextResponse.redirect(new URL('/', request.url))
-// };
-
-
 export { default } from 'next-auth/middleware'
 
 export const config = {
   matcher: [ 
     '/prerequisite',
-//     '/healthboard',
-//     '/healthboard/:path',
+    '/healthboard',
+    '/healthboard/:path',
   ]
 }
